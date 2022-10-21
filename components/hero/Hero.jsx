109384b1232ade22ad1082fa4e9ccc76e2405cec
Mark-Nelovka <87484591+Mark-Nelovka@users.noreply.github.com>
2022-10-21 22:52:17 +0300

    add styles for video

--- components/hero/Hero.jsx
+++ components/hero/Hero.jsx
@@ -1,19 +1,21 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+import s from "./Hero.module.css";
 
 export const Hero = () => {
-  const [width, setWidth] = useState(0);
-  const handleResize = () => setWidth(document.documentElement.clientWidth);
+  // const [width, setWidth] = useState(0);
+  // const handleResize = () => setWidth(document.documentElement.clientWidth);
 
-  useEffect(() => {
-    window.addEventListener("resize", handleResize);
-    setWidth(document.documentElement.clientWidth);
-    return () => window.removeEventListener("resize", handleResize);
-  }, [width]);
+  // useEffect(() => {
+  //   window.addEventListener("resize", handleResize);
+  //   setWidth(document.documentElement.clientWidth);
+  //   return () => window.removeEventListener("resize", handleResize);
+  // }, [width]);
 
   return (
     <section>
       <video
-        width={width}
+        className={s.video}
+        playsInline
         autoPlay
         loop
         muted
