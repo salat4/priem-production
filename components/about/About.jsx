a073a4a5e9510ecbbcce2f2bf880667fa5746e22
MashtalerOlha <87094132+MashtalerOlha@users.noreply.github.com>
2022-10-23 22:39:34 +0300

    add about and contact

--- components/about/About.jsx
+++
@@ -1,7 +0,0 @@
-export const About = () => {
-  return (
-    <section>
-      <h1>About</h1>
-    </section>
-  );
-};
--- pages/ua/index.js
+++ pages/ua/index.js
@@ -1,13 +1,16 @@
-import Footer_en from "../../components/footer/footer_en/footer_en";
 import Footer_ua from "../../components/footer/footer_uk/footer_uk";
 import { HeaderUa } from "../../components/header";
 import { Hero } from "../../components/hero/Hero";
-Footer_ua
+import { About } from "../../components/about/aboutUA";
+import { Contacts } from "../../components/contacts/ContactUA";
+
 export default function Ua() {
   return (
     <>
       <HeaderUa />
       <Hero />
+      <About/>
+      <Contacts/>
       <footer>
         <Footer_ua></Footer_ua>
       </footer>
