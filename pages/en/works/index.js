63511ba38fe40c639c2e168cded18ec34d40a358
Dima Malik <mdima4266@gmail.com>
2022-10-31 13:10:25 +0200

    remove page works

--- pages/en/works/index.js
+++ pages/en/works/index.js
@@ -1,11 +1,11 @@
 import { HeaderEn } from "../../../components/header";
-import Works_en from "../../../components/works/works_en/works_en";
+// import Works_en from "../../../components/works/works_en/works_en";
 import Footer_en from "../../../components/footer/footer_en/footer_en";
  function Works({snapshot}) {
   return (
     <>
       <HeaderEn />
-      <Works_en snapshot={snapshot}/>
+      {/* <Works_en snapshot={snapshot}/> */}
       <footer>
         <Footer_en/>
       </footer>
@@ -16,17 +16,17 @@ import Footer_en from "../../../components/footer/footer_en/footer_en";
 export default Works
 
 
-export async function getStaticProps(){
-  const res = await fetch('http://localhost:8080/snapshot')
-  const snapshot = await res.json()
-  if (!snapshot){
-      return{ 
-          notFound: true
-      }
-  }
-  return{
-      props:{
-          snapshot
-      }
-  }   
-}
+// export async function getStaticProps(){
+//   const res = await fetch('http://localhost:8080/snapshot')
+//   const snapshot = await res.json()
+//   if (!snapshot){
+//       return{ 
+//           notFound: true
+//       }
+//   }
+//   return{
+//       props:{
+//           snapshot
+//       }
+//   }   
+// }
