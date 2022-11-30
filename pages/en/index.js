36169f51037e056755f23a8a4758e132cf6ba626
Dima Malik <mdima4266@gmail.com>
2022-12-01 00:55:27 +0200

    start work with HmM

--- pages/en/index.js
+++ pages/en/index.js
@@ -4,7 +4,7 @@ import { Hero } from "../../components/hero/Hero";
 import { About } from "../../components/about/aboutEN";
 import { Contacts } from "../../components/contacts/ContactEN";
 // import Team from "../../components/team/team";
-import { HmM } from "../../components/HmM";
+import  HmM_en  from "../../components/HmM/HmM_en/HmM_en";
 import { Partners } from "../../components/partners";
 import { PresWorks } from "../../components/PresentWork";
 export default function En() {
@@ -13,7 +13,7 @@ export default function En() {
       <HeaderEn />
       <Hero />
       <About />
-      <HmM />
+      <HmM_en />
       <Partners />
       <PresWorks />
       {/* <Team /> */}
