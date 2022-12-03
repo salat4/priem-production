0eea3f708f0cd3f1b327be3386af1b527eb379ff
Марк Неловка <87484591+Mark-Nelovka@users.noreply.github.com>
2022-12-03 15:26:51 +0200

    added backdrop

--- components/works/works_en/works_en.jsx
+++ components/works/works_en/works_en.jsx
@@ -3,6 +3,7 @@ import s from "../works.module.css";
 import container from "../../../styles/container.module.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Backdrop from "../../../General/Backdrop";
 import ReactPlayer from "react-player";
 // import { useRouter } from "next/router";
 const acces = "bbcf126b704644bb50aea408d498767c";
@@ -13,29 +14,34 @@ function Works_en({ snapshot }) {
   // const router = useRouter();
   const getVideo = () => {
     axios
-      .get("http://localhost:8080/video")
+      .get(
+        "https://vimeo.com/api/oembed.json?url=https%3A//vimeo.com/767582772&width=480&height=360"
+      )
       .then((res) => {
         console.log(res.data);
         const qwe = new DOMParser()
           .parseFromString(res.data.html, "text/html")
           .getElementsByTagName("iframe")[0];
+        console.log(qwe);
         // router.push(res.data.player_embed_url);
-        setVid(qwe);
+        setVid(true);
       })
       .catch((error) => console.log(error));
   };
 
-  // useEffect(() => {
-  //   let iframe = document.getElementById("#iframe");
-  //   let player = new Vimeo.Player(iframe);
-  //   player.on("play", function () {
-  //     console.log("Played the video");
-  //   });
+  useEffect(() => {
+    setVid(true);
 
-  //   player.getVideoTitle().then(function (title) {
-  //     console.log("title:", title);
-  //   });
-  // }, []);
+    //   let iframe = document.getElementById("#iframe");
+    //   let player = new Vimeo.Player(iframe);
+    //   player.on("play", function () {
+    //     console.log("Played the video");
+    //   });
+
+    //   player.getVideoTitle().then(function (title) {
+    //     console.log("title:", title);
+    //   });
+  }, []);
 
   // let i = 1;
   return (
@@ -45,62 +51,43 @@ function Works_en({ snapshot }) {
       >
         <button onClick={getVideo}>Get video</button>
         {vid && (
-          <ReactPlayer
-            url="https://player.vimeo.com/video/767582772"
-            // style={{
-            //   position: "absolute",
-            //   top: 0,
-            //   left: 0,
-            //   width: "100" + "vh",
-            //   height: "100" + "vh",
-            // }}
-            config={{
-              vimeo: {
-                playerOptions: {
-                  playsinline: true,
+          <>
+            {/* <Backdrop /> */}
+            <ReactPlayer
+              url="https://player.vimeo.com/video/767582772"
+              className={s.worksVideoQwe}
+              config={{
+                vimeo: {
+                  playerOptions: {
+                    playsinline: true,
+                    width: 200,
+                  },
                 },
-              },
-            }}
-          />
-          // <iframe
-          // style={{
-          //   position: "absolute",
-          //   top: 0,
-          //   left: 0,
-          //   width: "100%",
-          //   height: "100%",
-          // }}
-          //   frameBorder="0"
-          //   playsInline
-          //   allowFullScreen
-          //   controls={false}
-          //   autoPlay
-          //   loop
-          //   muted="false"
-          //   src={vid.src}
-          //   type="video/mp4"
-          // ></iframe>
+              }}
+            />
+          </>
         )}
-
-        {/* <ul className={s.works__list}>
-                    {snapshot.map((snap)=>(
-                        <li className={s.works__item} key = {i++}>
-                            <span className={s.works__image}>
-                                <Image src ={snap.url}
-                                    layout="fill"
-                                />
-                            </span>
-                            <div className={ s.works__box}>
-                                <p className={s.works__name}>{snap.name}</p>
-                                <p className={s.works__dop}>{snap.dop}</p>
-                            </div>
-                            {console.log(snap)}
-                        </li>
-
-                    ))}
-                </ul> */}
       </section>
     </>
   );
 }
 export default Works_en;
+// <iframe
+// style={{
+//   position: "absolute",
+//   top: 0,
+//   left: 0,
+//   width: "100%",
+//   height: "100%",
+// }}
+//   frameBorder="0"
+//   playsInline
+//   allowFullScreen
+//   controls={false}
+//   autoPlay
+//   loop
+//   muted="false"
+//   src={vid.src}
+//   type="video/mp4"
+// ></iframe>
+// )}
