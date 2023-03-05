27c619219c80740a5053de3f644257aa5814e435
Марк Неловка <87484591+Mark-Nelovka@users.noreply.github.com>
2023-03-05 02:39:01 +0200

    add request for cloudinary

--- pages/works/index.js
+++ pages/works/index.js
@@ -1,11 +1,46 @@
 import { Header } from "@/generalFunc";
+import axios from "axios";
 // import Works_en from "../../components/works/works_en/works_en";
 // import Footer_en from "../../components/footer/footer_en/footer_en";
 // import { Contacts } from "../../components/contacts/ContactEN";
-function Works({ res }) {
+import { CldImage, CldVideoPlayer } from "next-cloudinary";
+import { Video, Transformation } from "cloudinary-react";
+import { useEffect, useState } from "react";
+import "../../18next";
+
+function Works(props) {
+  const [first, setFirst] = useState(null);
+  useEffect(() => {
+    console.log(props.data);
+    setFirst(props.data);
+  }, [props]);
+
   return (
     <>
       <Header />
+      <div>
+        {/* <Video publicId="work-templates/IMG_0111_udcgyb">
+          <Transformation
+            color="#ffffff"
+            height="320"
+            quality="auto:good"
+            width="568"
+            crop="scale"
+          />
+        </Video> */}
+        {/* {first && (
+          <video
+            playsInline
+            autoPlay
+            loop
+            muted
+            type="video/mp4"
+            src={first[0].url}
+             width="200"
+          />
+        )} */}
+      </div>
+
       {/* <Works_en />
       <Contacts />
       <footer>
@@ -15,6 +50,16 @@ function Works({ res }) {
   );
 }
 export default Works;
+
+export async function getServerSideProps() {
+  const res = await axios("http://localhost:3000/api/hello");
+
+  return {
+    props: {
+      data: res.data.data.resources,
+    },
+  };
+}
 
 // export async function getStaticProps() {
 // const res = await fetch('http://localhost:8080/snapshot')
